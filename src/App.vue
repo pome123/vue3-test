c86e2807e8c67ce3f632bd0794bae3f7e9d23273
pome123 <norisan.123456.loveee@gmail.com>
2020-10-29 15:31:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+h1 {{ message }}
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+  // color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
